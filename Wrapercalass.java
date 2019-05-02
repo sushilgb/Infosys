85d89ec8cef74aa3a62325db3732d4179324ca71
sushil gourkar <you@example.com>
2019-05-03 01:19:28 +0530

    we add the class java file after made some changes

--- Wrapercalass.java
+++ Wrapercalass.java
@@ -10,19 +10,17 @@ public class Wrapercalass
 		List<Integer> col=new ArrayList<>();
 		col.add(1);
 		col.add(2);
-		col.add(3);
-		col.add(4);
-		col.add(5);
-		col.add(6);
-		col.add(7);
-		col.add(8);
-		col.add(9);
+		
 		for(int i: col)
 		{
 			if(i!=5)
 			{
 				System.out.println("got it");
 				System.exit(0);
+			}
+			else
+			{
+				System.out.println("fail");
 			}
 			else
 			{
